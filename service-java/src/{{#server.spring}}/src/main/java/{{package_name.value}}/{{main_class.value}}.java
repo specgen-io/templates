94c1ad7b4c439c3b1e5ef71dfcbe0023c41364bb
Vladimir Sapronov <vladimir@specgen.io>
2022-03-31 13:48:39 -0400

    service-java: Started using arguments jsonlib, swagger, etc

--- service-java/src/{{#server.spring}}/src/main/java/{{package_name.value}}/{{main_class.value}}.java
+++ service-java/src/{{#server.spring}}/src/main/java/{{package_name.value}}/{{main_class.value}}.java
@@ -2,13 +2,17 @@ package {{package_name.value}};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+{{#swagger.value}}
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+{{/swagger.value}}
 
+{{#swagger.value}}
 @EnableSwagger2
+{{/swagger.value}}
 @SpringBootApplication
-public class TestServiceJavaApplication {
+public class {{main_class.value}} {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestServiceJavaApplication.class, args);
+		SpringApplication.run({{main_class.value}}.class, args);
 	}
 }
