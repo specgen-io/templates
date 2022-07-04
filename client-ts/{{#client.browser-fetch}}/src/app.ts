05d90ce300de5a1d3dc8273f0a1a2a5e63a37164
Anastasiya Nislovskaya <anastasiya@specgen.io>
2022-07-04 20:06:50 +0300

    client-ts: Fixed build script and dependencies (#9)

--- client-ts/{{#client.browser-fetch}}/src/app.ts
+++
@@ -1,9 +0,0 @@
-import * as sample from './spec/sample';
-
-declare global {
-  interface Window {
-    sampleClient: (config: {baseURL: string}) => sample.SampleClient
-  }
-}
-
-window.sampleClient = sample.client
