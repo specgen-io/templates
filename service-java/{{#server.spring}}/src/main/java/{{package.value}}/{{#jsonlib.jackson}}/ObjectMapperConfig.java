8f34b311b21ec3c3fb1163411f60cb754959ba0f
Vladimir Sapronov <vladimir@specgen.io>
2022-09-18 00:35:52 -0400

    service-java: Updating templates

--- service-java/{{#server.spring}}/src/main/java/{{package.value}}/{{#jsonlib.jackson}}/ObjectMapperConfig.java
+++ service-java/{{#server.spring}}/src/main/java/{{package.value}}/{{#jsonlib.jackson}}/ObjectMapperConfig.java
@@ -2,14 +2,15 @@ package {{package.value}};
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.*;
-import {{package.value}}.json.Json;
+import {{package.value}}.json.CustomObjectMapper;
 
+@Configuration
 public class ObjectMapperConfig {
 	@Bean
 	@Primary
 	public ObjectMapper objectMapper() {
 		ObjectMapper objectMapper = new ObjectMapper();
-		Json.setupObjectMapper(objectMapper);
+		CustomObjectMapper.setup(objectMapper);
 		return objectMapper;
 	}
 }
