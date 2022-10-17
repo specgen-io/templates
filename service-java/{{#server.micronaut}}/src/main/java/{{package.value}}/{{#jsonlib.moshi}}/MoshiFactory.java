1d7b181a61719caf0d070513207cede58a8b9823
Anastasiya Nislovskaya <anastasiya@specgen.io>
2022-10-17 19:47:16 +0300

    Java service (#14)
    * java-service: Added Json bean creating
    
    * java-service: Upgraded specgen version
    
    * java-service: Fixed plugin version
    
    * java-service: Fixed specgen version

--- service-java/{{#server.micronaut}}/src/main/java/{{package.value}}/{{#jsonlib.moshi}}/MoshiFactory.java
+++ service-java/{{#server.micronaut}}/src/main/java/{{package.value}}/{{#jsonlib.moshi}}/MoshiFactory.java
@@ -2,15 +2,19 @@ package {{package.value}};
 
 import com.squareup.moshi.Moshi;
 import io.micronaut.context.annotation.*;
-import jakarta.inject.Singleton;
 import {{package.value}}.json.CustomMoshiAdapters;
 
 @Factory
 public class MoshiFactory {
-    @Singleton
+    @Bean
     public Moshi getMoshi() {
         var moshiBuilder = new Moshi.Builder();
 		CustomMoshiAdapters.setup(moshiBuilder);
         return moshiBuilder.build();
+    }
+
+    @Bean
+    public Json json() {
+        return new Json(objectMapper());
     }
 }
